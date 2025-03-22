d1522efa8622f806dc671180f2e5112c782ba007
Yuvraj Dhariwal <112430265+chiragyuvi@users.noreply.github.com>
2025-03-23 00:54:08 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("localhost:3307","root","","realestatephp");
+$con = mysqli_connect("localhost:3309","root","","realestatephp");
 	if (mysqli_connect_errno())
 	{
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
